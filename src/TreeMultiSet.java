35964a3b9a7596d2b391a595b39e66a61986db25
fern1394 <fernandesjaron@gmail.com>
2023-09-19 01:28:27 -0400

    Coded most of the treemultiset file.

--- src/TreeMultiSet.java
+++ src/TreeMultiSet.java
@@ -1,2 +1,32 @@
 public class TreeMultiSet {
+    private Tree tree;
+
+    public TreeMultiSet() {
+        this.tree = Tree();
+    }
+
+    public boolean add(java.lang.Object item) {
+        this.tree.insert(item);
+        return True
+    }
+
+    public void remove(java.lang.Object item) {
+        this.tree.delete_item(item);
+    }
+
+    public boolean contains(java.lang.Object item) {
+        return this.tree.contains(item);
+    }
+
+    public boolean is_empty(java.lang.Object item) {
+        return this.tree.is_empty();
+    }
+
+    public int count(java.lang.Object item) {
+        return this.tree.count(item);
+    }
+
+    public int size() {
+        return this.tree.length();
+    }
 }
